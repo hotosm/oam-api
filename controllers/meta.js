cb1e3f7bae24b97bbb4bc6b92700443db8f3cae9
Scisco <scisco7@gmail.com>
2015-05-15 11:34:43 -0400

    add remote meta to meta controller

--- controllers/meta.js
+++ controllers/meta.js
@@ -5,6 +5,8 @@
 'use strict';
 
 var _ = require('lodash');
+var request = require('request');
+var parse = require('wellknown');
 var bboxPolygon = require('turf-bbox-polygon');
 var Meta = require('../models/meta.js');
 
@@ -13,8 +15,7 @@ var Meta = require('../models/meta.js');
 * @param {Object} payload - Payload contains query paramters and their values
 * @param {Callback} cb - The callback that handles the response
 */
-module.exports.query = function(payload, cb) {
-
+module.exports.query = function (payload, cb) {
   // bounding box search | looks for bbox in payload
   if (_.has(payload, 'bbox')) {
     var bboxPattern = /(-?\d+(?:\.\d*)?,-?\d+(?:\.\d*)?,-?\d+(?:\.\d*)?),-?\d+(?:\.\d*)?/;
@@ -36,5 +37,45 @@ module.exports.query = function(payload, cb) {
   // Execute the search and return the result via callback
   Meta.find(payload, function (err, records) {
     cb(err, records);
+  });
+};
+
+/**
+* Add Meta Information from a provided URI. This function reads the remote json meta file
+* and adds the content to Meta model.
+* @param {String} remoteUri - a URI to the remote file
+* @param {Callback} cb - The callback that handles the response
+*/
+module.exports.addRemoteMeta = function (remoteUri, cb) {
+  // Check if the meta data is already added
+  Meta.findOne({meta_uri: remoteUri}, function (err, meta) {
+    if (err) {
+      return cb(err);
+    }
+
+    // if the meta file doesn't exist then add
+    if (meta === null) {
+      request(remoteUri, function (err, response, body) {
+        if (err) {
+          return cb(err);
+        }
+        if (response.statusCode === 200) {
+          var payload = JSON.parse(body);
+          payload.meta_uri = remoteUri;
+
+          // create a geojson object from footprint and bbox
+          payload.geojson = parse(payload.footprint);
+          payload.geojson.bbox = payload.bbox;
+
+          var record = new Meta(payload);
+          record.save(function (err, record) {
+            if (err) {
+              return cb(err);
+            }
+            cb(err, record.uuid + ' added!');
+          });
+        }
+      });
+    }
   });
 };
