9adc9ab0ea29772d991b1106b0a0d520885f8f50
Scisco <scisco7@gmail.com>
2015-05-15 10:55:20 -0400

    add jsdocs

--- controllers/meta.js
+++ controllers/meta.js
@@ -1,13 +1,21 @@
+/**
+* Meta controller module
+* @module controllers/meta.js
+*/
 'use strict';
 
 var _ = require('lodash');
 var bboxPolygon = require('turf-bbox-polygon');
 var Meta = require('../models/meta.js');
 
+/**
+* Query Meta model. Implements all protocols supported by /meta endpoint
+* @param {Object} payload - Payload contains query paramters and their values
+* @param {Callback} cb - The callback that handles the response
+*/
 module.exports.query = function(payload, cb) {
 
-  // bounding box search
-  // look for bbox in payload
+  // bounding box search | looks for bbox in payload
   if (_.has(payload, 'bbox')) {
     var bboxPattern = /(-?\d+(?:\.\d*)?,-?\d+(?:\.\d*)?,-?\d+(?:\.\d*)?),-?\d+(?:\.\d*)?/;
 
@@ -20,11 +28,12 @@ module.exports.query = function(payload, cb) {
 
       console.log(JSON.stringify(bboxPolygon(coordinates)));
 
-      // remove ul and lr from payload
+      // remove bbox from payload
       payload = _.omit(payload, 'bbox');
     }
   }
 
+  // Execute the search and return the result via callback
   Meta.find(payload, function (err, records) {
     cb(err, records);
   });
