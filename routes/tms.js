d0555213bbac0ffff11b629ea75e75f28ed23606
Scisco <scisco7@gmail.com>
2015-07-06 13:16:39 -0400

    tms model, route, controller

---
+++ routes/tms.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var _ = require('lodash');
+var Boom = require('boom');
+var tms = require('../controllers/tms.js');
+
+module.exports = [
+  {
+    method: 'POST',
+    path: '/tms',
+    handler: function (request, reply) {
+      if (!_.isEmpty(request.payload) && _.has(request.payload, 'uri')) {
+        tms.addUpdate(request.payload, function (err, record) {
+          if (err) {
+            console.log(err);
+            return reply(Boom.badRequest(err));
+          }
+
+          return reply(record);
+        });
+      } else {
+        var err = Boom.create(
+          400,
+          'There is an Error. Fields missing.',
+          { timestamp: Date.now() }
+        );
+        return reply(Boom.badRequest(err));
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/tms',
+    handler: function (request, reply) {
+      var payload = {};
+
+      if (request.query) {
+        payload = request.query;
+      }
+
+      tms.query(payload, request.page, request.limit, function (err, records, count) {
+        if (err) {
+          console.log(err);
+          return reply(err.message);
+        }
+
+        request.count = count;
+        return reply(records);
+      });
+    }
+  }
+];
