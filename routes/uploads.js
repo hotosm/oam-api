7282b14a2c1f65ec8bf52e00611d5a0f36801651
Anand Thakker <vestibule@anandthakker.net>
2015-08-07 13:20:18 -0400

    Add initial docs

--- routes/uploads.js
+++ routes/uploads.js
@@ -3,6 +3,11 @@
 var Boom = require('boom');
 
 module.exports = [
+  /**
+   * @api {get} /uploads List uploads of currently authenticated user.
+   * @apiGroup uploads
+   * @apiSuccess {Object[]} results
+   */
   {
     method: 'GET',
     path: '/uploads',
@@ -14,6 +19,20 @@ module.exports = [
       });
     }
   },
+  /**
+   * @api {post} /uploads Add an upload to the queue
+   * @apiGroup uploads
+   *
+   * @apiParam {Object} uploaderInfo
+   * @pariParam {string} uploaderInfo.name
+   * @pariParam {string} uploaderInfo.email
+   * @apiParam {Object} contactInfo
+   * @pariParam {string} contactInfo.name
+   * @pariParam {string} contactInfo.email
+   * @apiParam {Object[]} scenes
+   * @apiParam {Object} scenes.metadata The OAM metadata
+   * @apiParam {string[]} scenes.urls The image URLs
+   */
   {
     method: 'POST',
     path: '/uploads',
@@ -24,12 +43,15 @@ module.exports = [
       }
     },
     handler: function (request, reply) {
+      // TODO: validate request
+
       var db = request.server.plugins.db.connection;
       var uploads = db.collection('uploads');
-      console.log('uploads', uploads);
       uploads.insert([request.payload], function (err, result) {
         request.log(['debug'], result);
         if (err) { return reply(Boom.wrap(err)); }
+        // TODO: kick off the job
+
         reply('Success');
       });
     }
