dabe3638438084044c3f3aee2e178aa72a0f911a
Anand Thakker <vestibule@anandthakker.net>
2015-08-15 00:04:05 -0400

    Add default port

--- .build_scripts/docker/run.sh
+++ .build_scripts/docker/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-touch local.env && source local.env && docker run -it -p $PORT --env-file=local.env --net=\"host\" -v $(pwd):/local oam-uploader-api $1
+touch local.env
+source local.env
+PORT=${PORT:-3000}
+docker run -it -p $PORT --env-file=local.env --net=\"host\" -v $(pwd):/local oam-uploader-api $1
