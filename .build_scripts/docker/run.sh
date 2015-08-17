640a40fcf2bb4f7cad7f2f601cde3b582013b4d1
Anand Thakker <vestibule@anandthakker.net>
2015-08-17 19:28:47 -0400

    Add docs on the docker setup

--- .build_scripts/docker/run.sh
+++ .build_scripts/docker/run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# This script runs the docker container for this application, by:
+#  - passing in the environment variables defined in `local.env` (creating an
+#    empty one if necessary)
+#  - exposing the $PORT (default: 3000), on which the app listens
+#  - setting the container to use the host OS's networking stack (better
+#    performance, worse isolation)
+#  - mounting the current directory--which should be the project root--as
+#    `/local`.
+#
+#  The intent of the last item is that the app can be setup (npm install) and
+#  run directly from its location on the host OS, but using the container as its
+#  environment to make dependency management trivial.
+
 touch local.env
 source local.env
 PORT=${PORT:-3000}
