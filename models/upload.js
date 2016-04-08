5c91537d99b8b35c63dbc16b936631e63a3d37f5
Daniel da Silva <daniel@developmentseed.org>
2016-04-08 12:57:15 +0100

    Solves tms validation issue

--- models/upload.js
+++ models/upload.js
@@ -13,7 +13,7 @@ var sceneSchema = Joi.object().keys({
   sensor: Joi.string(),
   acquisition_start: Joi.date().required(),
   acquisition_end: Joi.date().required(),
-  tms: Joi.string().uri(),
+  tms: Joi.string(),
   urls: Joi.array().items(Joi.string().uri({scheme: ['http', 'https']}))
     .min(1).required()
 });
