f4f9f87a5fa8cbb8b143a8eeb2009e13a0c8124c
Seth Fitzsimmons <seth@mojodna.net>
2017-03-21 17:26:14 -0700

    Indentation grumble grumble

--- worker/queue.js
+++ worker/queue.js
@@ -76,70 +76,70 @@ JobQueue.prototype._setupQueries = function _setupQueries () {
 // main loop
 JobQueue.prototype._mainloop = function mainloop () {
   return this.images
-    // look for an unprocessed image that hasn't been claimed, and (atomically)
-    // mark it as claimed by this worker
-    .findOneAndUpdate({
-      status: 'initial'
-    }, this.update.jobClaimed, { returnOriginal: false })
-    .then((result) => {
-      if (!result.value) {
-        // no jobs left; try to shut down.
-        // avoid race condition by making sure our state wasn't changed from
-        // 'working' to something else (by the server) before we actually quit.
-        return this.workers.updateOne(this.query.myself, this.update.stopping)
-          .then((result) => {
-            // failed to set our state, so continue processing
-            if (result.modifiedCount === 0) { return this._mainloop(); }
-            // we're in the clear - clean up and exit
-            return this.cleanup();
-          })
-          .catch(this.cleanup.bind(this));
-      }
-
-      // we got a job!
-      var image = result.value;
-      log(['info'], 'Processing job', image);
-
-      return this.db.collection('uploads')
-        // find the upload / scene that contains this image
-        .findOne({ 'scenes.images': image._id })
-        .then(function (upload) {
-          var found;
-
-          upload.scenes.forEach(function (scene, i) {
-            scene.images
-              .filter(id => image._id.equals(id))
-              .forEach(function (id) {
-                var key = [upload._id, i, uuidV4()].join('/');
-
-                // now that we have the scene, we can process the image
-                found = processImage(scene, image.url, key);
-              });
-          });
-
-        if (found) { return found; }
-        // this should never happen
-        throw new Error('Could not find the scene for image ' + image._id);
-      })
-      .then((processed) => {
-        // mark the job as finished
-        return this.images.findOneAndUpdate(result.value, this.update.jobFinished(processed));
-      })
-      .then(() => {
-        // update this worker's timestamp
-        return this.workers.updateOne(this.query.myself, this.update.lastJobTimestamp);
-      })
-      // keep going
-      .then(this._mainloop.bind(this))
-      .catch((error) => {
-        log(['error'], error);
-        return this.images.findOneAndUpdate(result.value, this.update.jobErrored(error))
-        .then(() => {
-          this.workers.updateOne(this.query.myself, this.update.lastJobTimestamp);
+  // look for an unprocessed image that hasn't been claimed, and (atomically)
+  // mark it as claimed by this worker
+  .findOneAndUpdate({
+    status: 'initial'
+  }, this.update.jobClaimed, { returnOriginal: false })
+  .then((result) => {
+    if (!result.value) {
+      // no jobs left; try to shut down.
+      // avoid race condition by making sure our state wasn't changed from
+      // 'working' to something else (by the server) before we actually quit.
+      return this.workers.updateOne(this.query.myself, this.update.stopping)
+        .then((result) => {
+          // failed to set our state, so continue processing
+          if (result.modifiedCount === 0) { return this._mainloop(); }
+          // we're in the clear - clean up and exit
+          return this.cleanup();
         })
-        .then(this._mainloop.bind(this));
+        .catch(this.cleanup.bind(this));
+    }
+
+    // we got a job!
+    var image = result.value;
+    log(['info'], 'Processing job', image);
+
+    return this.db.collection('uploads')
+    // find the upload / scene that contains this image
+    .findOne({ 'scenes.images': image._id })
+    .then(function (upload) {
+      var found;
+
+      upload.scenes.forEach(function (scene, i) {
+        scene.images
+          .filter(id => image._id.equals(id))
+          .forEach(function (id) {
+            var key = [upload._id, i, uuidV4()].join('/');
+
+            // now that we have the scene, we can process the image
+            found = processImage(scene, image.url, key);
+          });
       });
+
+      if (found) { return found; }
+      // this should never happen
+      throw new Error('Could not find the scene for image ' + image._id);
+    })
+    .then((processed) => {
+      // mark the job as finished
+      return this.images.findOneAndUpdate(result.value, this.update.jobFinished(processed));
+    })
+    .then(() => {
+      // update this worker's timestamp
+      return this.workers.updateOne(this.query.myself, this.update.lastJobTimestamp);
+    })
+    // keep going
+    .then(this._mainloop.bind(this))
+    .catch((error) => {
+      log(['error'], error);
+      return this.images.findOneAndUpdate(result.value, this.update.jobErrored(error))
+      .then(() => {
+        this.workers.updateOne(this.query.myself, this.update.lastJobTimestamp);
+      })
+      .then(this._mainloop.bind(this));
     });
+  });
 };
 
 JobQueue.prototype.cleanup = function cleanup (err) {
