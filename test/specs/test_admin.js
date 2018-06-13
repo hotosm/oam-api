4df59b71d54464b973d4557193e9edff5c407ebb
Ankita Khurana <ankita0096@gmail.com>
2018-06-13 17:40:57 +0530

    Test using Stubs 2

--- test/specs/test_admin.js
+++ test/specs/test_admin.js
@@ -9,7 +9,22 @@ const sandbox = sinon.sandbox.create();
 
 const tempAdmin = {
   name: 'test',
-  password: 'test'
+  password: 'test',
+  token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Mjc1OTExMDYsImRhdGEiOnsibmFtZSI6ImFua2l0YSIsInBhc3N3b3JkIjoiYW5raXRhIn0sImlhdCI6MTUyNzU4NzUwNn0.uhIta1PCdGoGCurHm4IczIru1W8l6TPCPkBi9d993Es'
+};
+
+const config = require('../../config');
+const Hapi = require('hapi');
+const authentication = require('../../plugins/authentication.js');
+const routes = require('../../routes/admin.js');
+
+const getServer = () => {
+  const server = new Hapi.Server();
+  server.connection({ port: 4000 });
+  return server.register(authentication).then(() => {
+    server.route(routes);
+    return server;
+  });
 };
 
 describe('Admin', () => {
@@ -31,8 +46,26 @@ describe('Admin', () => {
   it('isValidAdmin should return Admin with valid JWT token', () => {
     const admin = {name: tempAdmin.name, password: tempAdmin.name};
     sandbox.stub(Admin, 'findOne').returns(Promise.resolve(admin));
-    Admin.isValidAdmin(tempAdmin.name, tempAdmin.name, (result) => {
+    Admin.isValidAdmin(tempAdmin.name, tempAdmin.password, (result) => {
       expect(result).to.exist;
+    });
+  });
+  it('POST /admin should return Admin with correct credentials', () => {
+    const admin = {name: tempAdmin.name, password: tempAdmin.password, token: tempAdmin.token};
+    sandbox.stub(Admin, 'findOne').returns(Promise.resolve(admin));
+    var request = {
+      method: 'POST',
+      url: config.apiEndpoint + '/admin',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      payload: 'name=' + tempAdmin.name + '&password=' + tempAdmin.password
+    };
+    getServer().then(server => {
+      server.inject(request, function (response) {
+        expect(response.statusCode).to.equal(200);
+      });
+      server.stop();
     });
   });
 });
